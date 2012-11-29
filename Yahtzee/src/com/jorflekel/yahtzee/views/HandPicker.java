88cc05571a2749d5f515264a34cdf57062ae41d1
Matt Jordan <mjordan@mines.edu>
2012-11-29 10:39:29 -0700

    Fixed that merge problem.

--- Yahtzee/src/com/jorflekel/yahtzee/views/HandPicker.java
+++ Yahtzee/src/com/jorflekel/yahtzee/views/HandPicker.java
@@ -6,27 +6,9 @@ import android.widget.LinearLayout;
 
 public class HandPicker extends LinearLayout {
 
-	public HandPicker(Context context) {
-		super(context);
-		// TODO Auto-generated constructor stub
-	}
-
 	public HandPicker(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
-	}
-
-	public HandPicker(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
