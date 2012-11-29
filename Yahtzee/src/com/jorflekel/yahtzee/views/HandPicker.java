127bfd65bd3f1f2635fa1935a8ac293788276e12
Matt Jordan <mjordan@mines.edu>
2012-11-29 10:34:32 -0700

    Fixed some problems.

---
+++ Yahtzee/src/com/jorflekel/yahtzee/views/HandPicker.java
@@ -0,0 +1,32 @@
+package com.jorflekel.yahtzee.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class HandPicker extends LinearLayout {
+
+	public HandPicker(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HandPicker(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HandPicker(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
